fd45b343cbbfb033eb1dc4e55d4c3091119dc3b7
Noah Shanaberger <noah@thenoah.party>
2022-02-26 04:47:09 -0800

    Refining the spider

--- pkg/spider/spider.go
+++ pkg/spider/spider.go
@@ -1,8 +1,10 @@
 package spider
 
 import (
+	"bytes"
 	"errors"
-	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -30,6 +32,7 @@ type Site struct {
 type CrawledSite struct {
 	*Site
 	CrawledAt time.Time
+	Response  *http.Response
 }
 
 // Spider defines an instance of a web crawler.
@@ -37,6 +40,9 @@ type Spider struct {
 	OnCrawl     CrawledSiteHandler
 	Logger      *log.Logger
 	WorkerCount uint
+	SendDelay   *time.Duration
+	CrawlDelay  *time.Duration
+	ShouldCrawl ShouldCrawlURLHandler
 	state       struct {
 		*sync.RWMutex
 		current SpiderState
@@ -49,6 +55,9 @@ type Spider struct {
 // CrawledSiteHandler is a closure type that defines a function that is called upon a spider crawling a site.
 type CrawledSiteHandler func(site CrawledSite, spider *Spider)
 
+// ShouldCrawlURLHandler is a closure type that defines a function that is called upon to check whether or not the spider should crawl a url.
+type ShouldCrawlURLHandler func(foundAt, url *url.URL, spider *Spider) bool
+
 // New creates a new spider
 // workerCount defines how many workers for both crawling and handling crawled sites.
 func New(workerCount uint, args ...interface{}) (*Spider, error) {
@@ -58,7 +67,10 @@ func New(workerCount uint, args ...interface{}) (*Spider, error) {
 
 	spider := &Spider{
 		OnCrawl:        func(site CrawledSite, spider *Spider) {},
+		ShouldCrawl:    func(foundAt, url *url.URL, spider *Spider) bool { return true },
 		Logger:         log.Default(),
+		SendDelay:      nil,
+		CrawlDelay:     nil,
 		WorkerCount:    workerCount,
 		sitesWaitGroup: &sync.WaitGroup{},
 		state: struct {
@@ -78,12 +90,35 @@ func New(workerCount uint, args ...interface{}) (*Spider, error) {
 		if onCrawl, ok := arg.(func(site CrawledSite, spider *Spider)); ok {
 			spider.OnCrawl = onCrawl
 		}
+
+		if shouldCrawl, ok := arg.(func(foundAt, url *url.URL, spider *Spider) bool); ok {
+			spider.ShouldCrawl = shouldCrawl
+		}
+
+		if duration, ok := arg.(time.Duration); ok {
+			if spider.SendDelay == nil {
+				spider.SendDelay = &duration
+			} else {
+				spider.CrawlDelay = &duration
+			}
+		}
+	}
+
+	if spider.SendDelay == nil {
+		duration := time.Millisecond
+		spider.SendDelay = &duration
+	}
+
+	if spider.CrawlDelay == nil {
+		duration := time.Second / 2
+		spider.CrawlDelay = &duration
 	}
 
 	return spider, nil
 }
 
-func (s *Spider) Start() {
+// Start will start the spider.
+func (s *Spider) Start() error {
 	s.state.Lock()
 	defer s.state.Unlock()
 	if s.state.current == StateStopped {
@@ -96,7 +131,10 @@ func (s *Spider) Start() {
 			go s.resultsWorker(i)
 		}
 		s.state.current = StateCrawling
+		return nil
 	}
+
+	return errors.New("cannot start spider that is already started")
 }
 
 // Stop safely stops the spider.
@@ -109,9 +147,10 @@ func (s *Spider) Stop() {
 	s.state.current = StateStopped
 }
 
+// SendSitesMap allows you to externally send urls to the spider for handling
 func (s *Spider) SendSitesMap(sites map[string]Site) (err error) {
 	for _, site := range sites {
-		time.Sleep(time.Millisecond)
+		time.Sleep(*s.SendDelay)
 		s.state.Lock()
 		if s.state.current != StateCrawling {
 			s.state.Unlock()
@@ -124,9 +163,10 @@ func (s *Spider) SendSitesMap(sites map[string]Site) (err error) {
 	return
 }
 
+// SendSites allows you to externally send urls to the spider for handling
 func (s *Spider) SendSites(sites ...Site) (err error) {
 	for _, site := range sites {
-		time.Sleep(time.Millisecond)
+		time.Sleep(*s.SendDelay)
 		s.state.Lock()
 		if s.state.current != StateCrawling {
 			s.state.Unlock()
@@ -142,7 +182,7 @@ func (s *Spider) SendSites(sites ...Site) (err error) {
 // Send allows you to externally send urls to the spider for handling
 func (s *Spider) Send(urls ...string) (err error) {
 	for _, url := range urls {
-		time.Sleep(time.Millisecond)
+		time.Sleep(*s.SendDelay)
 		s.state.Lock()
 		if s.state.current != StateCrawling {
 			s.state.Unlock()
@@ -155,57 +195,77 @@ func (s *Spider) Send(urls ...string) (err error) {
 	return
 }
 
-func (s *Spider) log(worker, message string) {
-	s.Logger.Printf("[%s] - %s\n", worker, message)
-}
-
 func (s *Spider) siteWorker(id int) {
-	workerId := fmt.Sprintf("Crawl-%d", id)
+	// workerId := fmt.Sprintf("Crawl-%d", id)
 	for site := range s.sites {
 		client := http.Client{}
 		resp, err := client.Get(site.URL)
 		if err != nil {
 			continue
 		}
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+		body, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		if err != nil {
-			s.log(workerId, err.Error())
 			continue
 		}
 
-		thisURL, err := url.Parse(site.URL)
+		resp.Body = io.NopCloser(bytes.NewBuffer(body))
+
+		doc, err := goquery.NewDocumentFromReader(
+			ioutil.NopCloser(
+				bytes.NewBuffer(body),
+			),
+		)
+
 		if err != nil {
 			continue
 		}
-		thisURLString := thisURL.String()
 
-		foundSites := map[string]Site{}
+		url, err := url.Parse(site.URL)
+		if err != nil {
+			continue
+		}
 
-		doc.Find("[href]").Each(
-			func(i int, sel *goquery.Selection) {
-				if value, exists := sel.Attr("href"); exists {
-					currentURL, err := thisURL.Parse(value)
-					if err != nil {
-						return
-					}
-					currentURLString := currentURL.String()
-
-					foundSites[currentURLString] = Site{URL: currentURLString, FoundAt: &thisURLString}
-				}
-			},
+		go s.SendSitesMap(
+			s.findURLs(url, doc),
 		)
-		go s.SendSitesMap(foundSites)
-		s.results <- CrawledSite{Site: &site, CrawledAt: time.Now()}
+
+		s.results <- CrawledSite{Site: &site, CrawledAt: time.Now(), Response: resp}
+		time.Sleep(*s.CrawlDelay)
 	}
 
 	s.sitesWaitGroup.Done()
 }
 
 func (s *Spider) resultsWorker(id int) {
-	workerId := fmt.Sprintf("Result-%d", id)
+	// workerId := fmt.Sprintf("Result-%d", id)
 	for result := range s.results {
-		s.Logger.Println(workerId, *result.Site)
 		s.OnCrawl(result, s)
 	}
+}
+
+func (s *Spider) findURLs(url *url.URL, doc *goquery.Document) map[string]Site {
+	foundSites := map[string]Site{}
+	urlString := url.String()
+
+	doc.Find("[href]").Each(
+		func(_ int, sel *goquery.Selection) {
+			if value, exists := sel.Attr("href"); exists {
+				currentURL, err := url.Parse(value)
+				if err != nil {
+					return
+				}
+
+				if !s.ShouldCrawl(url, currentURL, s) {
+					return
+				}
+
+				currentURLString := currentURL.String()
+				foundSites[currentURLString] = Site{URL: currentURLString, FoundAt: &urlString}
+			}
+		},
+	)
+
+	return foundSites
 }
