4a8066538abbeb08a97f5d741fd70a3fe13e09a7
Noah Shanaberger <noah@thenoah.party>
2022-02-25 22:59:31 -0800

    Fix a channel error
    I was waiting on the wrong worker pool which, under certain circumstances, could end up leading to crashes due to sending over a closed channel

--- pkg/spider/spider.go
+++ pkg/spider/spider.go
@@ -3,6 +3,7 @@ package spider
 import (
 	"errors"
 	"fmt"
+	"log"
 	"sync"
 	"time"
 )
@@ -30,13 +31,14 @@ type CrawledSite struct {
 // Spider defines an instance of a web crawler.
 type Spider struct {
 	OnCrawl CrawledSiteHandler
+	Logger  *log.Logger
 	state   struct {
 		*sync.RWMutex
 		current SpiderState
 	}
-	sites            chan Site
-	results          chan CrawledSite
-	resultsWaitGroup *sync.WaitGroup
+	sites          chan Site
+	sitesWaitGroup *sync.WaitGroup
+	results        chan CrawledSite
 }
 
 // CrawledSiteHandler is a closure type that defines a function that is called upon a spider crawling a site.
@@ -44,9 +46,13 @@ type CrawledSiteHandler func(site CrawledSite, spider *Spider)
 
 // New creates a new spider
 // workerCount defines how many workers for both crawling and handling crawled sites.
-func New(workerCount uint, onCrawl CrawledSiteHandler) (*Spider, error) {
+func New(workerCount uint, logger *log.Logger, onCrawl CrawledSiteHandler) (*Spider, error) {
 	if workerCount == 0 {
 		return nil, errors.New("cannot create a spider with zero workers")
+	}
+
+	if logger == nil {
+		logger = log.Default()
 	}
 
 	if onCrawl == nil {
@@ -54,10 +60,11 @@ func New(workerCount uint, onCrawl CrawledSiteHandler) (*Spider, error) {
 	}
 
 	spider := &Spider{
-		OnCrawl:          onCrawl,
-		sites:            make(chan Site, workerCount),
-		results:          make(chan CrawledSite, workerCount),
-		resultsWaitGroup: &sync.WaitGroup{},
+		OnCrawl:        onCrawl,
+		Logger:         logger,
+		sites:          make(chan Site, workerCount),
+		sitesWaitGroup: &sync.WaitGroup{},
+		results:        make(chan CrawledSite, workerCount),
 		state: struct {
 			*sync.RWMutex
 			current SpiderState
@@ -69,8 +76,8 @@ func New(workerCount uint, onCrawl CrawledSiteHandler) (*Spider, error) {
 
 	for i := 0; i < int(workerCount); i++ {
 		go spider.siteWorker(i)
+		spider.sitesWaitGroup.Add(1)
 		go spider.resultsWorker(i)
-		spider.resultsWaitGroup.Add(1)
 	}
 
 	return spider, nil
@@ -81,7 +88,7 @@ func (s *Spider) Stop() {
 	s.state.Lock()
 	defer s.state.Unlock()
 	close(s.sites)
-	s.resultsWaitGroup.Wait()
+	s.sitesWaitGroup.Wait()
 	close(s.results)
 	s.state.current = StateStopped
 }
@@ -89,6 +96,7 @@ func (s *Spider) Stop() {
 // Send allows you to externally send urls to the spider for handling
 func (s *Spider) Send(urls ...string) (err error) {
 	for _, url := range urls {
+		time.Sleep(time.Millisecond)
 		s.state.Lock()
 		if s.state.current != StateCrawling {
 			s.state.Unlock()
@@ -104,16 +112,18 @@ func (s *Spider) Send(urls ...string) (err error) {
 func (s *Spider) siteWorker(id int) {
 	workerId := fmt.Sprintf("Crawl-%d", id)
 	for site := range s.sites {
-		fmt.Println(workerId, site.URL)
+		s.Logger.Println(workerId, site.URL)
+		time.Sleep(time.Second * 10)
+		s.results <- CrawledSite{Site: &site}
 	}
+
+	s.sitesWaitGroup.Done()
 }
 
 func (s *Spider) resultsWorker(id int) {
 	workerId := fmt.Sprintf("Result-%d", id)
 	for result := range s.results {
-		fmt.Println(workerId, result)
+		s.Logger.Println(workerId, result)
 		s.OnCrawl(result, s)
 	}
-
-	s.resultsWaitGroup.Done()
 }
